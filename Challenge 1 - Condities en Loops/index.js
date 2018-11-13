cc99a62fbf19f1e08ebf4aeac9a3b357dc34b8af
Damion White <damion.dewit@outlook.com>
2018-11-13 09:45:05 +0100

    forget the 'var' keyword

--- Challenge 1 - Condities en Loops/index.js
+++ Challenge 1 - Condities en Loops/index.js
@@ -1,6 +1,6 @@
 //  Create 1000 random numbers
 for (var i = 0; i < 1000; i++) {
-  num = Math.floor(Math.random() * 1000);
+  var num = Math.floor(Math.random() * 1000);
 
   //  If the remainder === 0, log the number
   if (num % 3 === 0) {
